097e9d2f704d30116e7c820bac9a97a53139beaf
Hongkaibin <1341541192@qq.com>
2020-04-30 16:55:00 +0800

    删除README.md

---
+++ src/test/java/testSpringUtils.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:appliciationContext.xml")
+public class testSpringUtils {
+
+    @Test
+    public void testSpringUtils() {}
+
+}
