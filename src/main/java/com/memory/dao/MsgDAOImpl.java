95ec88a97150561a60d2ea14fb361e77214128b0
cunkoulaocai <30389241+cunkoulaocai@users.noreply.github.com>
2020-05-05 15:52:31 +0800

    修改聊天信息模块

--- src/main/java/com/memory/dao/MsgDAOImpl.java
+++ src/main/java/com/memory/dao/MsgDAOImpl.java
@@ -3,10 +3,13 @@ package com.memory.dao;
 import com.memory.pojo.Memory;
 import com.memory.pojo.Msg;
 import com.memory.pojo.Tag;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
@@ -55,13 +58,14 @@ public class MsgDAOImpl implements MsgDAO{
     }
 
     @Override
+    @Transactional(propagation = Propagation.SUPPORTS)
     public List<Msg> getUnReadMsgList(Integer acceptUserId) {
+//        SELECT * from msg where msg_action=0 and receive_id=18
         Session session = hibernateTemplate.getSessionFactory().getCurrentSession();
-        String hql = String.format("from Msg where Msg.msgAction=0 and Msg.receiveUser.id=%d",acceptUserId);
-        System.out.println(hql);
-        Query query = session.createQuery(hql);
-        List<Msg> list = query.list();
-
+        String sql = "SELECT * from msg where msg_action=0 and receive_id=18";
+        SQLQuery sqlQuery = session.createSQLQuery(sql).addEntity(Msg.class);
+        List<Msg> list = sqlQuery.list();
+        System.out.println("list about" + list);
         return list;
     }
 }
